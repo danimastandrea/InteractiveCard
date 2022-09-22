bfb379dff3c19eb2359c60665f8a00abf51f1947
Dani Mastandrea <dvmastandrea@gmail.com>
2022-09-22 00:49:20 -0300

    mobile

--- index.js
+++ index.js
@@ -1,38 +1,4 @@
 
-/* 
-cardName.addEventListener("change", mostrar)
-
- function mostrar(){
-
-    let nameVar= document.getElementById('cardName').value;
-    
-    document.getElementById('name').innerHTML=nameVar
-
-    }
-
-  
-cardNumber.addEventListener("change", mostrar)
-
-function mostrar(){
-
-   let numberVar= document.getElementById('cardNumber').value;
-   
-   document.getElementById('number').innerHTML=numberVar
-
-   }
-
-   
-cardNumber.addEventListener("change", mostrar)
-
-function mostrar(){
-
-   let numberVar= document.getElementById('cardNumber').value;
-   
-   document.getElementById('number').innerHTML=numberVar
-
-   }
-
- */  
    
    cardName.addEventListener("change", mostrar)
    cardNumber.addEventListener("change", mostrar)
@@ -57,4 +23,8 @@ function mostrar(){
 
    }
 
+function enviado (){
+ document.getElementById('msg').style.display= 'block';
+
+}
   
